4af57c02ce45f9315b6c1e834cb2303f2140dff3
Steve Lamb <steve@zenlogic.co.uk>
2018-03-07 18:55:39 +0000

    allowed changing of the Clear button.

--- src/numbers.js
+++ src/numbers.js
@@ -4,6 +4,7 @@ import Square from './square';
 
 class Numbers extends React.Component {
     render(){
+        const noneButton = this.props.noneButton ? this.props.noneButton : 'Clear';
         var numbers = [], num;
         for(num = Number(this.props.from); num <= Number(this.props.to);num++){
             numbers.push(num);
@@ -12,10 +13,9 @@ class Numbers extends React.Component {
             return(
                 <Square key={value} value={value} onClick={() => this.props.onClick(value)}/>
             )
-        })
-        ;
+        });
         return(
-            <div>{numbersJSX}<Square key="none" value="Clear" onClick={() => this.props.onClick('none')}/></div>
+            <div>{numbersJSX}<Square key="none" value={noneButton} onClick={() => this.props.onClick('none')}/></div>
         )
     }
 }
