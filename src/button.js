eaa3d09582528ecb8ae4fcc59dcbaed853cd574a
Steve Lamb <steve@zenlogic.co.uk>
2018-03-08 15:48:53 +0000

    tried using this.props.children to populate button contents

--- src/button.js
+++ src/button.js
@@ -4,7 +4,7 @@ import React from 'react';
 class Button extends React.Component {
     render(){
         return (
-            <button className={this.props.className} onClick={this.props.onClick}>{this.props.value}</button>
+            <button className={this.props.className} onClick={this.props.onClick}>{this.props.children}</button>
         )    
     }
 }
